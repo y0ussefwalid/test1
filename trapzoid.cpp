6f88b28ed5365abc976390fc4235b33d15385e78
y0ussefwalid <162761283+y0ussefwalid@users.noreply.github.com>
2024-03-23 20:37:14 +0200

    comments added

--- trapzoid.cpp
+++ trapzoid.cpp
@@ -3,20 +3,27 @@ using namespace std;
 int main() {
 	int width, height;
 	double test;
+	//used as an indicator if the program should add space or not
 	int space = 0;
 	cout << "enter width: "; cin >> width;
 	cout << "enter height: "; cin >> height;
+	// to check if it is possible to draw a trapzoid
 	test = width / height;
 	if (test >= 2) {
+		// height is the number of lines will be drawn
 		for (int i{ height }; i >= 0; i--) {
+			//width is the number of stars will be drawn
 			for (int stars = width; stars >= 0; stars--) {
 				cout << '*';
 			}
 			cout << "\n";
+			//to add spaces after new line 
 			for (int j = 0; j <= space; j++) {
 				cout << ' ';
 			}
+			//we delete two stars after every line completed
 			width -= 2;
+			//number of spaces increase when stars decrease
 			space += 1;
 		}
 	}
